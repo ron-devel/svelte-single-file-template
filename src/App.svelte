8099fd7e43565e4a3afcb88c68a7080c853fac7b
Ron Bermejo <ronbermejo@gmail.com>
2024-06-22 13:19:41 +0200

    Ran: npx @svelte-add/tailwindcss@1.0.15

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,18 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+
 
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      <img src="{viteLogo}" class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Counter></Counter>
   </div>
 
   <p>
@@ -28,7 +24,11 @@
   </p>
 </main>
 
-<style>
+
+<script>import "./app.css";
+import svelteLogo from "./assets/svelte.svg";
+import viteLogo from "/vite.svg";
+import Counter from "./lib/Counter.svelte";</script><style>
   .logo {
     height: 6em;
     padding: 1.5em;
