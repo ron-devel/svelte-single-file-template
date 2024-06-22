41f2545e20e484b500341729d9b76f4adf6ab5f4
Ron Bermejo <ronbermejo@gmail.com>
2024-06-22 13:33:43 +0200

    Adds skeleton UI from
    https://www.skeleton.dev/docs/get-started

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,37 @@
-
+<script>
+  import "./app.css";
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
 
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="{viteLogo}" class="logo" alt="Vite Logo" />
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Vite + Svelte (TM)</h1>
 
   <div class="card">
     <Counter></Counter>
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
-
-<script>import "./app.css";
-import svelteLogo from "./assets/svelte.svg";
-import viteLogo from "/vite.svg";
-import Counter from "./lib/Counter.svelte";</script><style>
+<style>
   .logo {
     height: 6em;
     padding: 1.5em;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+// @ts-check
+import { join } from 'path';
+
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
+    darkMode: 'class',
+
+    content: ["./src/**/*.{html,js,svelte,ts}",
+        join(require.resolve(
+            '@skeletonlabs/skeleton'),
+            '../**/*.{html,js,svelte,ts}'
+        )
+    ],
 
     theme: {
         extend: {}
     },
 
-    plugins: [require("@tailwindcss/typography")]
+    plugins: [
+        require("@tailwindcss/typography"),
+        skeleton
+    ]
 };
